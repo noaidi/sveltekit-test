4e1dac0a885a451587eaca3b256c4701761ec1c6
Seunghyuk Choi <noaidi@gmail.com>
2021-11-21 22:10:23 +0900

    ...

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,4 +1,6 @@
 export async function get({params}) {
+	// const wait = (timeToDelay) => new Promise((resolve) => setTimeout(resolve, timeToDelay));
+	// await wait(2000);
 	return {
 		status: 200,
 		body: {
